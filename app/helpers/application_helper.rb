4760a6af66de5723d96a8509bf2568d535b7e6df
juaruizra <j_ruiz963@hotmail.com>
2016-07-15 20:59:00 -0500

    agregando vistas de login y sign up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def alert_class_for (flash_type)
+    {
+      :SUCCESS => 'alert.s-success',
+      :error => 'alert-danger',
+      :alert => 'alert-warning',
+      :notice => 'alert-info'
+    }
+    [flash_type.to_sym] || flash_type.to_s
+  end
+
+
 end
