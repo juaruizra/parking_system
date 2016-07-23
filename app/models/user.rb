07276da1bd458d08a510186993a557ecba3332be
juaruizra <j_ruiz963@hotmail.com>
2016-07-22 20:52:09 -0500

    reseteo bd para unique de email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 has_many :vehicles
 
-validates :first_name, length: { minimum: 2 }, presence: true, format: /\A[A-Z]+\z/
-validates :last_name, length: { minimum: 2 }, presence: true, format: /\A[A-Z]+\z/
+validates :first_name, presence: true, format: /\A[A-Z]+\z/
+validates :last_name, presence: true, format: /\A[A-Z]+\z/
 validates :email, presence: true, uniqueness: true, format:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 validates :password, length: { in: 6..20 }
 validates_date :birthdate, :on_or_before => lambda { Date.current }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723010447) do
+ActiveRecord::Schema.define(version: 20160723013431) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "location_latitude"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20160723010447) do
     t.string   "last_sign_in_ip"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "vehicles", force: :cascade do |t|
