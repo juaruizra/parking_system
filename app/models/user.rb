810518769807739fa6a16dbe3a8feb730b78ed74
juaruizra <j_ruiz963@hotmail.com>
2016-07-25 18:30:28 -0500

    validaciones de usuarios y callback de admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,17 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-has_many :vehicles
+  has_many :vehicles
 
-validates :first_name, presence: true, format: /\A[A-Z]+\z/
-validates :last_name, presence: true, format: /\A[A-Z]+\z/
-validates :email, presence: true, uniqueness: true, format:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-validates :password, length: { in: 6..20 }
-validates_date :birthdate, :on_or_before => lambda { Date.current }
+  validates :first_name, presence: true# format:{with: /\A[A-Z]+\z/} #validar inicio con mayusculas
+  validates :last_name, presence: true # format: {with: /\A[A-Z]+\z/}
+  validates :email, presence: true, uniqueness: true, format:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates :password, length: { in: 6..20 }
+#validates_date :birthdate, :on_or_before => lambda { Date.current }
+
+  before_save do
+      self.is_admin = false
+  end
 
 
 
