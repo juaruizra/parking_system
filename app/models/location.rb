788ecb0c2bc64d79cb4acb4b9f2e0a8a3db57616
juaruizra <j_ruiz963@hotmail.com>
2016-07-28 20:37:45 -0500

    listo locations y vehicle 2

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,15 +4,19 @@ class Location < ActiveRecord::Base
   validates :address, presence: true
 
 
+#
+# after_validation :reverse_geocode
+# reverse_geocoded_by :latitude, :longitude do |obj,results|
+#   if geo = results.first
+#     obj.address   = geo.city+','+geo.postal_code+','+geo.country_code
+# #    obj.zipcode = geo.postal_code
+# #    obj.country = geo.country_code
+#   end
+# end
 
-after_validation :reverse_geocode
-reverse_geocoded_by :latitude, :longitude do |obj,results|
-  if geo = results.first
-    obj.address   = geo.city+','+geo.postal_code+','+geo.country_code
-#    obj.zipcode = geo.postal_code
-#    obj.country = geo.country_code
-  end
-end
+after_validation :geocode, if: ->(obj){obj.address.present? && obj.address_changed?}
+geocoded_by :address
+reverse_geocoded_by :latitude, :longitude
 
 
 
